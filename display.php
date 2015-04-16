86a814f26123cfce9fd8d57b4bdda7a8b9c76940
mhsstemworks <keith.imada@mauihigh.org>
2015-04-15 15:24:02 -1000

    4.15.2015
    Display page now working
    - Updated CSS
    
    Renamed intex.html -> main.html
    
    Removed display.html

--- display.php
+++ display.php
@@ -1,20 +1,54 @@
-<!DOCTYPE html>
-<html> 
-<meta http-equiv="refresh" content="5" >
-<head> 
-<title>A BASIC HTML FORM</title> 
-<body> 
-<p>
-<?php
-echo date("D M d, Y G:i a");
-?> 
-</p> 
+<!DOCTYPE HTML>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="display.css">
+<meta charset="utf-8" http-equiv="refresh" content="5" >
 
-<p1>
-<form method="POST" action=''>
-<input type="submit" name="button1" value="Next">
-</form>
+<title>Display</title>
+<script>
+function time() {
 
+	var clock = document.querySelector('.time');
+	
+	// But there is a little problem
+	// we need to pad 0-9 with an extra
+	// 0 on the left for hours, seconds, minutes
+	
+	var pad = function(x) {
+		return x < 10 ? '0'+x : x;
+	}
+	
+	var ticktock = function() {
+		var d = new Date();
+		
+		var h = pad( d.getHours() );
+		var m = pad( d.getMinutes() );
+		var s = pad( d.getSeconds() );
+		
+		var current_time = [h,m,s].join(':');
+		
+		clock.innerHTML = current_time;
+		
+	}
+	
+	ticktock();
+	
+	// Calling ticktock() every 1 second
+	setInterval(ticktock, 1000);
+
+}
+</script>
+</head>
+
+<body onload="time()">
+
+<div class="table">
+<table> 
+<tr> 
+<th>Name:</th>
+</tr>
+<tr>
+<td>
 <?php
 
 $c = 0;
@@ -50,26 +84,31 @@ fclose($dfp);
 fclose($dfile); 
 fclose($fp);
 
-
-
 $file_handle = fopen("sdlist.csv", "r"); 
   
 while (!feof($file_handle) ) { 
   
 $line_of_text = fgetcsv($file_handle, 1024); 
   
-print $line_of_text[0] . " " . $line_of_text[1] . " " . $line_of_text[2] ."<BR>"; 
+print $line_of_text[0] . " " . $line_of_text[1] . " " . "<br>"; 
   
 } 
   
 fclose($file_handle); 
 
+
 ?>
 
+</td>
+</tr>
+</table>
 
+<form method="POST" action=''>
+<input type="submit" name="button1" class="button1" value="Next">
+</form>
 
 <form method="POST" action=''> 
-<input type="submit" name="button2" value="Delete" onClick="return confirm('Are you sure you want to delete the lists');">
+<input type="submit" name="button2" class="button2" value="Delete" onClick="return confirm('Are you sure you want to delete the lists');">
 </form>
 <?php
 if (isset($_POST['button2']))
@@ -80,10 +119,17 @@ $sfp = fopen("list.csv", "w");
 }
 ?>
 
+</div>
 
-</p1>
+<div id="date" class="date">
+<script>
+	var d = new Date();
+	document.getElementById("date").innerHTML = d.toDateString();
+</script>
+</div>
 
+<div class="time">
+00:00:00
+</div>
 </body>
-
-
 </html>
